f6ebd1365c19e5f623e5288e201cc0cbe05c1d59
Ramir Holubov <ramir.holubov@gmail.com>
2023-03-21 17:43:26 +0200

    Fixing typos in backend file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,8 @@
-backend "azurerm" {
-  resource_group_name = var.bkrg
-  storage_account_name = var.bkstrg
-  container_name = var.bkcontainer
-  key = var.bkstrgkey
+terraform {
+  backend "azurerm" {
+    resource_group_name  = var.bkrg
+    storage_account_name = var.bkstrg
+    container_name       = var.bkcontainer
+    key                  = var.bkstrgkey
+  }
 }
