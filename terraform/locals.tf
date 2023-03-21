f35deb3cf4633f72a826d7dbee02f721eb596376
Ramir Holubov <ramir.holubov@gmail.com>
2023-03-22 01:44:15 +0200

    Updating infrastructure file

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 locals {
   tags = { "environment" : var.environment }
   pattern_name = "${lower(var.project)}-${lower(var.environment)}-${lower(var.region)}"
-  storage_account_pattern_name = "${lower(var.project)}-${lower(var.environment)}-${lower(var.region)}"
+  storage_account_pattern_name = "${lower(var.project)}${lower(var.environment)}${lower(var.region)}"
 }
